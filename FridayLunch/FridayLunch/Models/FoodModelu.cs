895e69d161d439d5765b9cfcade51822d2934388
JustasG <justas.grekavicius@gmail.com>
2018-01-25 12:21:03 +0200

    Renamed models, removed files from tracking

---
+++ FridayLunch/FridayLunch/Models/FoodModelu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FridayLunch.Models
+{
+    public class FoodMenu
+    {
+        public string MainDish { get; set; }
+        public bool IsIncludedSideDish { get; set; }
+        public string SideDish { get; set; }
+        public string Soup { get; set; }
+        public bool IsIncludedDrink { get; set; }
+        public string Drink { get; set; }
+        
+    }
+}
