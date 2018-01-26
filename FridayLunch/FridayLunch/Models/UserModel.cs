50e3c9a5aa5e0a64e83494d6f69811316d66d054
JustasG <justas.grekavicius@gmail.com>
2018-01-26 14:58:05 +0200

    Added functionality to Login

--- FridayLunch/FridayLunch/Models/UserModel.cs
+++ FridayLunch/FridayLunch/Models/UserModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +9,18 @@ namespace FridayLunch.Models
 {
     public class UserModel
     {
-        public int Id { get; set; }
+        public ObjectId Id { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public int Floor { get; set; }
-        public string UserName { get; set; }
+
+        public string Username { get; set; }
+
         public string Password { get; set; }
+
         public bool IsAdmin { get; set; }
     }
 }
